92a09e55dd3dc8a9b114c95f7f12f485d1beea82
Chan Kha Vu <hav4ik@users.noreply.github.com>
2023-03-17 23:10:21 -0700

    remove face detection

--- Code/Client/Face.py
+++ Code/Client/Face.py
@@ -4,14 +4,15 @@ import cv2
 import numpy as np
 class  Face:
     def __init__(self):
-        self.recognizer = cv2.face.LBPHFaceRecognizer_create()
-        self.recognizer.read('Face/face.yml')
-        self.detector = cv2.CascadeClassifier("Face/haarcascade_frontalface_default.xml")
-        self.name = self.Read_from_txt('Face/name')
+        # self.recognizer = cv2.face.LBPHFaceRecognizer_create()
+        # self.recognizer.read('Face/face.yml')
+        # self.detector = cv2.CascadeClassifier("Face/haarcascade_frontalface_default.xml")
+        # self.name = self.Read_from_txt('Face/name')
     def Read_from_txt(self, filename):
-        file1 = open(filename + ".txt", "r")
-        list_row = file1.readlines()
+        # file1 = open(filename + ".txt", "r")
+        # list_row = file1.readlines()
         list_source = []
+        """
         for i in range(len(list_row)):
             column_list = list_row[i].strip().split("\t")
             list_source.append(column_list)
@@ -19,9 +20,11 @@ class  Face:
             for j in range(len(list_source[i])):
                 list_source[i][j] = str(list_source[i][j])
         file1.close()
+        """
         return list_source
 
     def Save_to_txt(self, list, filename):
+        return
         file2 = open(filename + '.txt', 'w')
         for i in range(len(list)):
             for j in range(len(list[i])):
@@ -30,9 +33,10 @@ class  Face:
             file2.write('\n')
         file2.close()
     def getImagesAndLabels(self,path='Face'):
-        imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
+        #imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
         faceSamples=[]
         labels = []
+        """
         for imagePath in imagePaths:
             if os.path.split(imagePath)[-1].split(".")[1]=="jpg":
                 id = int(os.path.split(imagePath)[-1].split(".")[0])
@@ -42,8 +46,10 @@ class  Face:
                 for (x,y,w,h) in faces:
                     faceSamples.append(gray[y:y+h,x:x+w])
                     labels.append(id)
+        """
         return faceSamples,labels
     def trainImage(self):
+        return
         faces, labels = self.getImagesAndLabels()
         self.recognizer.train(faces, np.array(labels))
         self.recognizer.write('Face/face.yml')
@@ -53,7 +59,8 @@ class  Face:
         try:
             if sys.platform.startswith('win') or sys.platform.startswith('darwin'):
                 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-                faces = self.detector.detectMultiScale(gray,1.2,5)
+                # faces = self.detector.detectMultiScale(gray,1.2,5)
+                faces = []
                 if len(faces)>0 :
                     for (x,y,w,h) in faces:
                         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
